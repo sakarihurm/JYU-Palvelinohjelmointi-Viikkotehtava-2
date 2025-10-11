887c9c61d7834defb09a7bc2143a53210505e346
Hurmerinta Sakari Paavo Johannes <sakari.j.hurmerinta@student.jyu.fi>
2025-10-11 15:38:20 +0300

    lisätty lomakkeen esitäyttö koodista

--- app.py
+++ app.py
@@ -11,6 +11,22 @@ def peli():
 @app.route('/lauta', methods=['POST', 'GET'])
 def lauta():
     koko = request.values.get("lauta", "")
-    koko = int(koko)
-    hello = "asd"
-    return render_template('pohja.xhtml', hello=hello, koko=koko)
+    pelaaja1 = request.values.get("p1", "")
+    pelaaja2 = request.values.get("p2", "")
+    print(pelaaja1)
+    print(pelaaja2)
+    
+    try:
+        koko = int(koko)
+    except:
+        koko = 8
+        virhe = True
+
+    if koko > 16:
+        koko = 16
+
+    if koko < 8:
+        koko = 8
+        
+    virhe = False
+    return render_template('pohja.xhtml', koko=koko, virhe=virhe, pelaaja1=pelaaja1, pelaaja2=pelaaja2)
