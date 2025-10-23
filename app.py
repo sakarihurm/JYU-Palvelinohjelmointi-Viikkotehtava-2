b71b7efcac6712da9e1b879c53d1456a6c3f2215
Hurmerinta Sakari Paavo Johannes <sakari.j.hurmerinta@student.jyu.fi>
2025-10-23 17:32:30 +0300

    muokattu url muodostaminen urlencodella

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request, Response, render_template, url_for, redirect
 import urllib.request, json
+from urllib.parse import urlencode
+
 import os
 app = Flask(__name__)
 
@@ -21,9 +23,13 @@ def lauta():
     global base_url
     virhe = False
 
-    with urllib.request.urlopen("https://europe-west1-ties4080.cloudfunctions.net/vt2_taso1") as response:
-        data = json.load(response)
-    print(data)
+    try: 
+        with urllib.request.urlopen("https://europe-west1-ties4080.cloudfunctions.net/vt2_taso1") as response:
+            data = json.load(response)
+        print(data)
+    except Exception as e:
+        print("Virhe haettaessa dataa:", e)
+        data = {"min":8, "max":16, "first":"white", "balls": "top-to-bottom"}
 
     try:
         koko = request.args.get('lauta')
@@ -71,21 +77,23 @@ def tarkistaNimet(pelaaja1, pelaaja2):
 @app.route('/vt2/piilota', methods=['POST'])
 def piilota():
     try:
-        rivi = int(request.values.get("rivi"))
-        sarake = int(request.values.get("sarake"))
-        koko = int(request.form.get('koko', data["min"]))
-        p1 = request.form.get('p1', '')
-        p2 = request.form.get('p2', '')
-
-        piilotetut.add((rivi, sarake))
-
-        url = "/palauta?rivi="+ str(rivi) +"&sarake=" + str(sarake) +"&koko="+ str(koko) +"&p1="+ p1 +"&p2="+ p2
+        tiedot = {
+            "rivi": int(request.values.get("rivi")),
+            "sarake": int(request.values.get("sarake")),
+            "koko": int(request.form.get('koko', data["min"])),
+            "p1": request.form.get('p1', ''),
+            "p2": request.form.get('p2', '')
+        }
+       
+        piilotetut.add((tiedot["rivi"], tiedot["sarake"]))
+        
+        url = "/palauta?" + urlencode(tiedot)
         haku = base_url + url
 
     except Exception as e:
         print("piilotus virhe", e)
 
-    return Response(render_template('pohja.xhtml', koko=koko, virhe=False, pelaaja1=p1, pelaaja2=p2, haku=haku, first=data["first"], balls=data["balls"], piilotetut=piilotetut, palautetut=palautetut), content_type="application/xhtml+xml; charset=utf-8")
+    return Response(render_template('pohja.xhtml', koko=tiedot["koko"], virhe=False, pelaaja1=tiedot["p1"], pelaaja2=tiedot["p2"], haku=haku, first=data["first"], balls=data["balls"], piilotetut=piilotetut, palautetut=palautetut), content_type="application/xhtml+xml; charset=utf-8")
 
 @app.route('/vt2/palauta', methods=['GET'])
 def palauta():
